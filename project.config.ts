9c417604ec4a780c97a66537cd87c865a516f90d
bluelovers <codelovers@users.sourceforge.net>
2017-12-07 04:53:32 +0800

    . (Step: 5)
    Step: 5
    
    [A ] new file:
    
    	.gitignore
    	.npmignore
    	lib/func.ts
    	package.json
    	project.config.ts

---
+++ project.config.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 2017/8/13/013.
+ */
+
+import * as path from 'path';
+
+export const project_root = path.join(__dirname);
+
+export const dist_root = path.join(project_root, 'dist');
+export const temp_root = path.join(project_root, 'test/temp');
+
+export default module.exports;
