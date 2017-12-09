fc7503cca1e54c32f3ee0cf9e8c7061c46e03b2a
bluelovers <codelovers@users.sourceforge.net>
2017-12-10 06:32:38 +0800

    . (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	lib/i18n.ts
    	lib/locales/index.ts
    	lib/locales/黑之魔王.ts
    	lib/str-width.ts
    	lib/zhnumber.ts

---
+++ lib/locales/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 2017/12/9/009.
+ */
+
+export const lang = 'zh';
+
+export const value = {
+	chapter_id: '第{{0}}話',
+	chapter_title: `$t(chapter_id, [{{0}}])　{{title}}`,
+
+	volume_id: '第{{0}}章',
+	volume_title: `$t(chapter_id, [{{0}}])：{{title}}`,
+};
+
+// @ts-ignore
+export default exports;
