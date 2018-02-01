8d7b0aa74d90d53eb24e29aabad515a9592dabb4
bluelovers <codelovers@users.sourceforge.net>
2018-02-02 06:13:06 +0800

    小說家下載

---
+++ src/site/syosetu/index.ts
@@ -0,0 +1,358 @@
+/**
+ * Created by user on 2017/12/22/022.
+ */
+
+import fetch from 'lets-fetch';
+import { trimFilename } from '../../../lib/func';
+import cheerioJSDOM, {} from '../../../lib/jsdom';
+import * as Promise from 'bluebird';
+import * as moment from 'moment-timezone';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import * as projectConfig from '../../../project.config';
+import { novelText } from '../../../lib/novel/text';
+import * as shortid from 'shortid';
+import { download_image } from '../image';
+import { json2md } from '../../../lib/fs/metamd';
+
+moment.fn.toJSON = function () { return this.format(); }
+
+export const IDKEY = 'syosetu';
+export const PATH_NOVEL_MAIN = path.join(projectConfig.dist_novel_root, IDKEY);
+
+export async function get_volume_list(url)
+{
+	{
+		let data = parseUrl(url);
+
+		if (!data.novel_id)
+		{
+			throw new ReferenceError();
+		}
+
+		url = makeUrl(data, true);
+	}
+
+	return await cheerioJSDOM(url)
+		.then(async function (dom)
+		{
+			let novel_title = dom.$('.novel_title').text();
+			let novel_author = dom.$('.novel_writername a').text();
+
+			let novel_desc = dom.$('#novel_ex').text();
+
+			let novel_publisher = IDKEY;
+
+			let url_data = parseUrl(dom.source_url);
+
+			let volume_list = [];
+
+			let currentVolume;
+
+			let table = dom.$('.index_box').find('> .chapter_title, .novel_sublist2');
+
+			let _cache_dates = [];
+
+			let novel_syosetu_id;
+
+			{
+				let m;
+				let dt = dom.$('#novel_footer .undernavi a[href*="txtdownload"]').attr('href');
+				if (m = dt.match(/ncode\/(\d+)/))
+				{
+					novel_syosetu_id = m[1];
+				}
+				else
+				{
+					throw new Error()
+				}
+			}
+
+			table
+				.each(function (index)
+				{
+					let tr = dom.$(this);
+
+					if (tr.is('.chapter_title'))
+					{
+						currentVolume = volume_list[volume_list.length] = {
+							volume_index: volume_list.length,
+							volume_title: tr.text().replace(/^\s+|\s+$/g, ''),
+							chapter_list: [],
+						};
+					}
+					else
+					{
+						let a = tr.find('.subtitle a');
+
+						tr.find('.long_update *').remove();
+						let chapter_date;
+
+						let dd = tr.find('.long_update').text().replace(/^\s+|\s+$/g, '');
+
+						if (dd)
+						{
+							chapter_date = moment(dd, 'YYYY/MM/DD HH:mm').local();
+
+							_cache_dates.push(chapter_date.unix());
+						}
+
+						let href = a.prop('href');
+
+						let data = parseUrl(href);
+
+						if (!data.chapter_id)
+						{
+							throw new Error()
+						}
+						else
+						{
+							data = {
+								url: null,
+								novel_pid: novel_syosetu_id as string,
+								chapter_id: data.chapter_id as string,
+							} as any;
+
+							href = makeUrl(data);
+
+							data.url = href;
+						}
+
+						currentVolume
+							.chapter_list
+							.push({
+								chapter_index: currentVolume.length,
+								chapter_title: a.text().replace(/^\s+|\s+$/g, ''),
+								chapter_id: data.chapter_id,
+								chapter_url: href,
+								chapter_url_data: data,
+								chapter_date,
+							})
+						;
+					}
+				})
+			;
+
+			_cache_dates.sort();
+
+			let novel_date = moment.unix(_cache_dates[_cache_dates.length - 1]).local();
+
+			return {
+				url: dom.source_url,
+				url_data,
+
+				novel_title,
+				novel_author,
+
+				novel_desc,
+				novel_date,
+				novel_publisher,
+
+				novel_syosetu_id,
+
+				volume_list,
+
+				checkdate: moment().local(),
+
+				imgs: [] as string[],
+			};
+		})
+		;
+}
+
+export function makeUrl(urlobj, bool?: boolean)
+{
+	if (urlobj.novel_pid && urlobj.chapter_id)
+	{
+		let cid = (!bool && urlobj.chapter_id) ? '&cid=' + urlobj.chapter_id : '';
+
+		return `https://ncode.syosetu.com/txtdownload/dlstart/ncode/${urlobj.novel_pid}/?no=${urlobj.chapter_id}&hankaku=0&code=utf-8&kaigyo=crlf`;
+	}
+
+	return `http://${urlobj.novel_r18 || 'ncode'}.syosetu.com/${urlobj.novel_id}`;
+}
+
+export function parseUrl(url: string)
+{
+	let urlobj = {
+		url: url,
+
+		novel_pid: null,
+		novel_id: null,
+		chapter_id: null,
+	};
+
+	url = url.toString();
+
+	let r: RegExp;
+	let m;
+
+	r = /txtdownload\/dlstart\/ncode\/(\d+)/;
+	if (m = r.exec(url))
+	{
+		urlobj.novel_pid = m[1];
+
+		return urlobj;
+	}
+
+	r = /\.syosetu\.com\/(n\w+)(?:\/?(\d+))?/;
+	if (m = r.exec(url))
+	{
+		urlobj.novel_id = m[1];
+		urlobj.chapter_id = m[2];
+
+		return urlobj;
+	}
+
+	return urlobj;
+}
+
+/**
+ * @param {string} url
+ */
+export async function download(url: string)
+{
+	{
+		let data = parseUrl(url);
+
+		if (!data.novel_id)
+		{
+			throw new ReferenceError();
+		}
+
+		url = makeUrl(data, true);
+	}
+
+	let novel = await get_volume_list(url);
+
+	//console.log(novel);
+
+	let idx = 0;
+
+	let path_novel = path.join(PATH_NOVEL_MAIN,
+		`${trimFilename(novel.novel_title)}_(${novel.url_data.novel_id})`
+	);
+
+	fetch.retry((tries) => tries <= 3);
+
+	let ret = await Promise
+		.mapSeries(novel.volume_list, function (volume, vid)
+		{
+			vid = vid.toString().padStart(4, '0') + '0';
+
+			let dirname = path.join(path_novel,
+				`${vid} ${trimFilename(volume.volume_title)}`
+				)
+			;
+
+			return Promise
+				.mapSeries(volume.chapter_list, async function (chapter)
+				{
+					chapter.chapter_index = (idx++);
+
+					let ext = '.txt';
+					let cid = chapter.chapter_index.toString().padStart(4, '0');
+
+					let pad = chapter.chapter_date.format('YYYYMMDDHHmm');
+
+					let file = path.join(dirname,
+						`${cid}_${trimFilename(chapter.chapter_title)}\.${pad}${ext}`
+					);
+
+					if (fs.existsSync(file))
+					{
+						console.log(`skip\n${volume.volume_title}\n${chapter.chapter_title}`);
+					}
+					else
+					{
+						console.log(`${chapter.chapter_title} ${pad}`);
+					}
+
+					let tryc = 0;
+
+					let fn = async function ()
+					{
+						tryc++;
+
+						return await fetch.single(chapter.chapter_url, {
+								type: 'text',
+								method: 'POST',
+							})
+							.catch(function (e)
+							{
+								if (tryc > 3)
+								{
+									return Promise.reject(e);
+								}
+
+								console.warn(`wait 25s\n${volume.volume_title}\n${chapter.chapter_title}`);
+
+								return new Promise(function (done)
+								{
+									setTimeout(done, 25000);
+								})
+									.then(fn)
+									;
+							})
+						;
+					};
+
+					let text = await fn()
+						.then(async function (text)
+						{
+							fs.outputFile(file, text);
+
+							return text;
+						})
+					;
+
+					return file;
+				})
+				;
+		})
+		.tap(ls =>
+		{
+			let file = path.join(path_novel,
+				`${trimFilename(novel.novel_title)}.${novel.url_data.novel_id}.json`
+				)
+			;
+
+			//console.log(ls);
+
+			return fs.outputJSON(file, novel, {
+				spaces: "\t",
+			});
+		})
+	;
+
+	let md = await json2md(novel, {
+		tags: [
+			IDKEY,
+		],
+	});
+
+	if (md)
+	{
+		md += `
+# options
+
+## ${IDKEY}
+
+- txtdownload_id: ${novel.novel_syosetu_id}
+
+## textlayout
+
+- allow_lf2: true
+
+`;
+
+		let file = path.join(path_novel, `README.md`);
+
+		await fs.outputFile(file, md);
+	}
+
+	return novel;
+}
+
+export default download;
+//export default exports;
