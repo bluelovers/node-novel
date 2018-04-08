29ded6accea31c615927f7c4cdd4a50b00128336
bluelovers <codelovers@users.sourceforge.net>
2018-04-08 22:13:22 +0800

    [/^【([^\n「」【】]+)】$/gm, '「$1」'],

--- lib/locales/回復術士のやり直し～即死魔法とスキルコピーの超越ヒール～.ts
+++ lib/locales/回復術士のやり直し～即死魔法とスキルコピーの超越ヒール～.ts
@@ -229,6 +229,8 @@ export const words: IWords[] = sublib._word_zh_all([
 
 	[/\n+(～[^\n]+～)\n+/gm, '\n\n\n$1\n\n'],
 
+	[/^【([^\n「」【】]+)】$/gm, '「$1」'],
+
 	...sublib.lazymarks[4],
 
 	...sublib.lazymarks[0],
