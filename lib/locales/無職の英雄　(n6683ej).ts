bedec57fa0aec219fa3ba04ea0f04468924b7026
bluelovers <codelovers@users.sourceforge.net>
2018-07-21 00:05:32 +0800

    [/[〝＂]([^〝〟「」『』＂\n]+)〟/g, '〈$1〉'],

--- lib/locales/無職の英雄　(n6683ej).ts
+++ lib/locales/無職の英雄　(n6683ej).ts
@@ -35,7 +35,7 @@ export const words: IWords[] = sublib._word_zh_all([
 	['鋼劍士|刚劍士', '鋼劍士'],
 	['莉莉婭|莉莉安', '莉莉婭'],
 
-	[/〝([^〝〟「」\n]+)〟/g, '〈$1〉'],
+	[/[〝＂]([^〝〟「」『』＂\n]+)〟/g, '〈$1〉'],
 
 	...sublib.lazymarks['class'],
 
