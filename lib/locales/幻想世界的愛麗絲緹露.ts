29ecfd777e364993a3c851eae78853d0b5c268db
bluelovers <codelovers@users.sourceforge.net>
2018-08-15 00:04:23 +0800

    .

---
+++ lib/locales/幻想世界的愛麗絲緹露.ts
@@ -0,0 +1,85 @@
+/**
+ * Created by user on 2017/12/21/021.
+ */
+
+import { sp, IWords, vMaybe, sublib } from './index';
+import * as StrUtil from 'str-util';
+import { _word_en } from './lib/index';
+
+/**
+ * 改成小說名字
+ */
+export const lang = '';
+
+/**
+ * 其他用途
+ *
+ * @type {{chapter_id: string; chapter_title: string; volume_id: string; volume_title: string}}
+ */
+export const value = {
+	chapter_id: '第{{0}}話',
+	chapter_title: `$t(chapter_id, [{{0}}])　{{title}}`,
+
+	volume_id: '第{{0}}章',
+	volume_title: `$t(chapter_id, [{{0}}])：{{title}}`,
+};
+
+/**
+ * 在這裡放此小說專屬的取代樣本
+ */
+export const words: IWords[] = sublib._word_zh_all([
+
+	['Guild|公会', '公会'],
+	['紫嫣|紫苑', '紫苑'],
+
+	['提露|緹露', '緹露'],
+
+	['伊莉亞|伊利亞', '伊莉亞'],
+
+	['艾莉諾亞?', '艾莉諾'],
+
+	['伯特蘭德|貝魯托朗|ベルトラン', '貝魯托朗'],
+
+	...sublib.lazymarks['class'],
+
+	...sublib.lazymarks[4],
+
+	_word_en(/\d+/g, function (...m)
+	{
+		return m[1] + StrUtil.toFullNumber(m[2]);
+	}),
+
+	_word_en(/[a-z]/ig, function (...m)
+	{
+		return m[1] + StrUtil.toFullEnglish(m[2]);
+	}),
+
+	...sublib.lazymarks[0],
+	...sublib.lazymarks[1],
+	...sublib.lazymarks[2],
+	...sublib.lazymarks[3],
+	...sublib.lazymarks[5],
+
+] as IWords[]);
+
+/**
+ * 需要人工確認的屏蔽字或錯字用語等等
+ */
+export const words_maybe: vMaybe = [
+
+	//'需要偵測的字',
+
+] as vMaybe;
+
+/**
+ * 分析取代完成後執行的代碼
+ *
+ * @param {string} text
+ * @returns {string}
+ */
+export function words_callback(text: string): string
+{
+	return text;
+}
+
+export default exports;
