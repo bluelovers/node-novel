ff2831a6c119f24c100e5916d763fa09a63bab84
bluelovers <codelovers@users.sourceforge.net>
2018-12-05 18:26:21 +0800

    [done] 這個勇者明明超強卻過分慎重

---
+++ bin/_run_epub.ts
@@ -0,0 +1,66 @@
+/**
+ * Created by user on 2018/10/6/006.
+ */
+
+import * as crossSpawn from 'cross-spawn-extra';
+import * as path from 'path';
+import gitDiffIDNovelID, { localesPath, searchLocalesID } from '../lib/git';
+import { freeGC } from '../lib/util';
+import ProjectConfig from '../project.config';
+import Promise = require('bluebird');
+import * as fs from 'fs-extra';
+import { array_unique } from '../lib/func';
+import novelInfo, { mdconf_parse, IMdconfMeta } from 'node-novel-info';
+import * as yargs from 'yargs';
+import { console } from 'debug-color2';
+import prettyuse = require('prettyuse');
+import * as novelGlobby from 'node-novel-globby';
+import * as iconv from 'iconv-jschardet';
+import { tw2cn_min, cn2tw_min, tableCn2TwDebug, tableTw2CnDebug } from 'cjk-conv/lib/zh/convert/min';
+
+let cli = yargs
+	.argv
+;
+
+let arr_ids = gitDiffIDNovelID(ProjectConfig.dist_novel_root);
+
+let _cache_file = path.join(
+	ProjectConfig.temp_root,
+	path.basename('_novel_run_git_diff.js') + '.json'
+);
+
+if (arr_ids.length == 0 && fs.existsSync(_cache_file))
+{
+	arr_ids = fs.readJSONSync(_cache_file);
+	console.info(`使用上次執行的目錄`, arr_ids);
+}
+
+if (arr_ids.length != 1)
+{
+	throw new Error();
+}
+
+Promise
+	.mapSeries(arr_ids, async function ({
+		pathMain,
+		novelID,
+	})
+	{
+		let cwd_path = path.join(ProjectConfig.dist_novel_root, pathMain, novelID);
+
+		let meta: IMdconfMeta;
+
+		console.log(pathMain, novelID);
+
+		return crossSpawn.async('npx', [
+			'novel-epub',
+			'--input', cwd_path,
+			'--output', path.join(ProjectConfig.temp_root, 'epub'),
+
+		], {
+			cwd: cwd_path,
+			stdio: 'inherit',
+		})
+	})
+	;
+
---
+++ lib/locales/この勇者が俺ＴＵＥＥＥくせに慎重すぎる.ts
@@ -0,0 +1,84 @@
+/**
+ * Created by user on 2017/12/21/021.
+ */
+
+import { sp, IWords, vMaybe, sublib } from './index';
+import * as StrUtil from 'str-util';
+import { _word_en, lazymarks, _word_jp1 } from './lib/index';
+
+/**
+ * 改成小說名字 (可留白 則自動設定為檔案名稱)
+ */
+export const lang = '';
+
+/**
+ * 在這裡放此小說專屬的取代樣本
+ */
+export const words_source: IWords[] = [
+
+	_word_jp1('阿莉亞|アリア|阿麗亞', '阿莉亞'),
+
+	_word_jp1('莉斯塔|麗絲塔', '莉斯塔'),
+
+];
+
+/**
+ * 實際使用的取代樣式
+ */
+export const words: IWords[] = sublib._word_zh_all([
+
+	...words_source,
+
+	...sublib.lazymarks['class'],
+	//...sublib.lazymarks['zh_cht'],
+
+	//...sublib.lazymarks['unit'],
+
+	...sublib.lazymarks[4],
+
+	...sublib.lazymarks['full_width_001'],
+	//...sublib.lazymarks['full_width_002'],
+
+	...sublib.lazymarks[0],
+	...sublib.lazymarks[1],
+	...sublib.lazymarks[2],
+	...sublib.lazymarks[3],
+	...sublib.lazymarks[5],
+
+] as IWords[]);
+
+/**
+ * 需要人工確認的屏蔽字或錯字用語等等
+ */
+export const words_maybe: vMaybe = [
+
+	//'需要偵測的字',
+
+] as vMaybe;
+
+/**
+ * 分析取代完成後執行的代碼
+ *
+ * @param {string} text
+ * @returns {string}
+ */
+export function words_callback(text: string): string
+{
+	return text;
+}
+
+/**
+ * 其他用途
+ *
+ * @deprecated
+ * @type {{chapter_id: string; chapter_title: string; volume_id: string; volume_title: string}}
+ */
+export const value = {
+	chapter_id: '第{{0}}話',
+	chapter_title: `$t(chapter_id, [{{0}}])　{{title}}`,
+
+	volume_id: '第{{0}}章',
+	volume_title: `$t(chapter_id, [{{0}}])：{{title}}`,
+};
+
+export default exports;
