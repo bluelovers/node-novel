eff0654a33fdc0ed4bf48d8ebd89bcc854377f0c
bluelovers <codelovers@users.sourceforge.net>
2018-12-31 04:17:08 +0800

    轉換繁體

---
+++ bin/_novel_source.ts
@@ -0,0 +1,137 @@
+/**
+ * Created by user on 2018/12/31/031.
+ */
+
+import { console } from 'debug-color2';
+import * as fs from 'fs-extra';
+import * as iconv from 'iconv-jschardet';
+import * as path from "path";
+import * as yargs from 'yargs';
+import { replace_name_list } from '../lib/fs/txt-rename';
+import gitDiffIDNovelID from '../lib/git';
+import { trimTxtLine } from '../lib/util';
+import ProjectConfig from '../project.config';
+import { _replace_list_001, getNovel, globNovel } from './lib/util';
+import Bluebird = require('bluebird');
+
+let cli = yargs
+	.argv
+;
+
+let arr_ids = gitDiffIDNovelID(ProjectConfig.dist_novel_root);
+
+let _cache_file = path.join(
+	ProjectConfig.temp_root,
+	path.basename(__filename) + '.json',
+);
+
+if (arr_ids.length == 1)
+{
+	fs.outputJSONSync(_cache_file, arr_ids);
+}
+else if (1)
+{
+	throw new Error();
+}
+else if (arr_ids.length == 0 && fs.existsSync(_cache_file))
+{
+	arr_ids = fs.readJSONSync(_cache_file);
+	console.info(`使用上次執行的目錄`, arr_ids);
+}
+
+Bluebird
+	.mapSeries(arr_ids, async function ({
+		pathMain,
+		novelID,
+	})
+	{
+		return getNovel({
+			pathMain,
+			novelID,
+
+			async callback(rowData)
+			{
+				let { file, currentFile, _last_empty, STAT_CACHE, len, index } = rowData;
+
+				let _t_old: string;
+				// @ts-ignore
+				_t_old = await fs.readFile(file);
+
+				if (_t_old.toString() === '')
+				{
+					_last_empty.push(currentFile);
+
+					//console.gray(currentFile, '此檔案無內容');
+
+					STAT_CACHE.skip++;
+
+					return currentFile;
+				}
+				else
+				{
+					if (_last_empty.length)
+					{
+						_last_empty
+							.forEach(function (currentFile)
+							{
+								console.red(currentFile, '此檔案無內容');
+							})
+						;
+
+						_last_empty = [];
+					}
+
+					let chk = iconv.detect(_t_old);
+
+					if (chk.encoding != 'UTF-8' && chk.encoding != 'ascii')
+					{
+						console.red(currentFile, '此檔案可能不是 UTF8 請檢查編碼或利用 MadEdit 等工具轉換', chk);
+					}
+				}
+
+				// @ts-ignore
+				_t_old = _t_old.toString();
+
+				let _text = _replace_list_001().reduce(function (name, data)
+				{
+					// @ts-ignore
+					return name.replace(...data);
+				}, _t_old);
+
+				let _t = trimTxtLine(_text);
+
+				let changed = _t != _t_old;
+
+				if (!_t.replace(/\xa0\s+　/g, ''))
+				{
+					STAT_CACHE.error++;
+
+					console
+						.yellow
+						.bgRed(currentFile, index, len)
+					;
+
+					await fs.outputFile(file, '');
+				}
+				else if (changed)
+				{
+					console[changed ? 'log' : 'red'](currentFile, index, len);
+
+					await fs.outputFile(file, _t);
+				}
+				else
+				{
+					console[changed ? 'log' : 'red'](currentFile, index, len);
+				}
+
+				if (changed)
+				{
+					STAT_CACHE.changed++;
+				}
+
+				return rowData.currentFile;
+			},
+		})
+			.then(globNovel)
+	})
+;
---
+++ bin/lib/util.ts
@@ -0,0 +1,254 @@
+/**
+ * Created by user on 2018/12/31/031.
+ */
+
+import Bluebird = require('bluebird');
+import prettyuse = require('prettyuse');
+import { console } from 'debug-color2';
+import * as fs from 'fs-extra';
+import * as novelGlobby from 'node-novel-globby/g';
+import { IMdconfMeta, mdconf_parse } from 'node-novel-info';
+import * as path from "path";
+import { zhRegExp } from '../../../../nodejs-yarn/regexp-workspaces/packages/regexp-cjk';
+import { _word_zh_all, lazymarks } from '../../lib/locales/lib/index';
+import ProjectConfig from '../../project.config';
+
+console.enabledColor = true;
+
+console.inspectOptions = console.inspectOptions || {};
+console.inspectOptions.colors = true;
+
+export interface IGetNovel<T = string>
+{
+	pathMain: string;
+	novelID: string;
+	globby_patterns: string[];
+	globby_options: any;
+	meta: IMdconfMeta;
+	cwd_path: string;
+	callback: IGetNovelCallback<T>;
+}
+
+export interface IGetNovelCallback<T>
+{
+	(data: {
+		file: string;
+		index: number;
+		len: number;
+		ext: string;
+		name: string;
+		file_dir: string;
+		currentFile: string;
+
+		inputOptions: IGetNovel<T>,
+
+		_last_empty: ICacheLastEmpty,
+		STAT_CACHE: ICacjeStat,
+	}): T
+}
+
+export interface ICacjeStat
+{
+	all: number,
+	file: number,
+	changed: number,
+	skip: number,
+	error: number,
+}
+
+export type ICacheLastEmpty = string[];
+
+export function getNovel<T>(inputOptions: {
+	pathMain: string,
+	novelID: string,
+
+	patterns?: string[],
+
+	callback: IGetNovelCallback<T>,
+})
+{
+	return Bluebird.resolve()
+		.then(async function ()
+	{
+		let { pathMain, novelID, patterns, callback } = inputOptions;
+
+		let cwd_path = path.join(ProjectConfig.dist_novel_root, pathMain, novelID);
+
+		let meta: IMdconfMeta;
+
+		if (fs.existsSync(path.join(cwd_path, 'README.md')))
+		{
+			meta = await fs.readFile(path.join(cwd_path, 'README.md'))
+				.then(mdconf_parse)
+		}
+		else if (fs.existsSync(path.join(cwd_path, 'meta.md')))
+		{
+			meta = await fs.readFile(path.join(cwd_path, 'meta.md'))
+				.then(mdconf_parse)
+		}
+
+		if (!meta)
+		{
+			throw new Error([pathMain, novelID].join())
+		}
+
+		let globby_patterns: string[] = patterns || undefined;
+		let globby_options: novelGlobby.IOptions = {
+			cwd: cwd_path,
+			useDefaultPatternsExclude: true,
+			absolute: true,
+		};
+
+		{
+			[globby_patterns, globby_options] = novelGlobby.getOptions(globby_patterns, globby_options);
+		}
+
+		if (patterns && patterns.length)
+		{
+			globby_patterns.push(...patterns);
+		}
+
+		return {
+			pathMain, novelID,
+
+			globby_patterns, globby_options,
+
+			meta,
+			cwd_path,
+
+			callback,
+		} as IGetNovel<T>
+	})
+	;
+}
+
+export function globNovel<T>(inputOptions: IGetNovel<T>)
+{
+	let {
+		pathMain, novelID,
+
+		globby_patterns, globby_options,
+
+		meta,
+		cwd_path,
+
+		callback,
+	} = inputOptions;
+
+	console.info(pathMain, novelID);
+
+	let TIME_LABEL = novelID;
+	console.time(TIME_LABEL);
+
+	let _last_empty: ICacheLastEmpty = [];
+
+	let STAT_CACHE: ICacjeStat = {
+		all: 0,
+		file: 0,
+		changed: 0,
+		skip: 0,
+		error: 0,
+	};
+
+	//console.log(globby_patterns);
+
+	return Bluebird
+		.mapSeries(novelGlobby
+			.globbyASync(globby_patterns, globby_options), function (file, index, len)
+		{
+			let ext = '.txt';
+
+			let name = path.basename(file, ext);
+			let file_dir = path.relative(cwd_path, path.dirname(file));
+
+			let currentFile = path.join(file_dir, name);
+
+			return callback({
+				file, index, len,
+
+				ext,
+				name,
+				file_dir,
+				currentFile,
+
+				inputOptions,
+
+				_last_empty,
+				STAT_CACHE,
+			})
+		})
+		.tap(function (ls)
+		{
+			if (_last_empty.length)
+			{
+				_last_empty
+					.forEach(function (currentFile)
+					{
+						console.grey(currentFile, '此檔案無內容');
+					})
+				;
+
+				_last_empty = [];
+			}
+
+			STAT_CACHE.all = ls.length;
+
+			STAT_CACHE.file = STAT_CACHE.all - STAT_CACHE.skip;
+
+			return ls;
+		})
+		.tap(function ()
+		{
+			console.debug(prettyuse());
+			console.debug(STAT_CACHE);
+		})
+		.finally(function ()
+		{
+			console.timeEnd(TIME_LABEL);
+		})
+		.tap(function ()
+		{
+			console.success(pathMain, novelID);
+		})
+}
+
+export let CACHE_REGEXP_LIST_001 = [] as [RegExp, string | ((...argv: string[]) => string)][];
+
+export function _replace_list_001()
+{
+	if (!CACHE_REGEXP_LIST_001.length)
+	{
+		CACHE_REGEXP_LIST_001 = [
+			..._word_zh_all([
+
+				...lazymarks['c000'],
+				...lazymarks['c050'],
+				...lazymarks['c100'],
+
+			]).map(function (data)
+			{
+				if (!(data[0] instanceof RegExp) || !(data[0] instanceof zhRegExp))
+				{
+					data[0] = new zhRegExp(data[0], 'ig');
+				}
+
+				return data;
+			}) as typeof CACHE_REGEXP_LIST_001,
+
+		];
+
+		//console.log(CACHE_REGEXP_LIST);
+	}
+
+	CACHE_REGEXP_LIST_001.forEach(data =>
+	{
+
+		// @ts-ignore
+		if ((data[0] instanceof RegExp) || (data[0] instanceof zhRegExp))
+		{
+			data[0].lastIndex = 0;
+		}
+	});
+
+	return CACHE_REGEXP_LIST_001;
+}
