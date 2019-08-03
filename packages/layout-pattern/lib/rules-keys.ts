ee39596da42a5bcfc6e7504265c4f2f51a73a45d
bluelovers <codelovers@users.sourceforge.net>
2019-08-04 06:35:51 +0800

    [output] 銜尾蛇的紀錄

--- packages/layout-pattern/lib/rules-keys.ts
+++ packages/layout-pattern/lib/rules-keys.ts
@@ -3,14 +3,16 @@ export const enum EnumRuleListKeys
 {
 	BASE_OLD = "base-old",
 	BASE_V2 = "base-v2",
+	DEMO_LF2_CHT = "demo.lf2.cht",
+	DEMO_LF2 = "demo.lf2",
 	DEMO = "demo",
 	DEMO_ZHT = "demo.zht",
 }
 
-export type IRuleListKey = EnumRuleListKeys.BASE_OLD | EnumRuleListKeys.BASE_V2 | EnumRuleListKeys.DEMO | EnumRuleListKeys.DEMO_ZHT | "base-old" | "base-v2" | "demo" | "demo.zht";
+export type IRuleListKey = EnumRuleListKeys.BASE_OLD | EnumRuleListKeys.BASE_V2 | EnumRuleListKeys.DEMO_LF2_CHT | EnumRuleListKeys.DEMO_LF2 | EnumRuleListKeys.DEMO | EnumRuleListKeys.DEMO_ZHT | "base-old" | "base-v2" | "demo.lf2.cht" | "demo.lf2" | "demo" | "demo.zht";
 
-export type IRuleListKey2 = EnumRuleListKeys.BASE_OLD | EnumRuleListKeys.BASE_V2 | EnumRuleListKeys.DEMO | EnumRuleListKeys.DEMO_ZHT;
+export type IRuleListKey2 = EnumRuleListKeys.BASE_OLD | EnumRuleListKeys.BASE_V2 | EnumRuleListKeys.DEMO_LF2_CHT | EnumRuleListKeys.DEMO_LF2 | EnumRuleListKeys.DEMO | EnumRuleListKeys.DEMO_ZHT;
 
-export type IRuleListKey3 = "base-old" | "base-v2" | "demo" | "demo.zht";
+export type IRuleListKey3 = "base-old" | "base-v2" | "demo.lf2.cht" | "demo.lf2" | "demo" | "demo.zht";
 
-export const RULE_LIST = [EnumRuleListKeys.BASE_OLD, EnumRuleListKeys.BASE_V2, EnumRuleListKeys.DEMO, EnumRuleListKeys.DEMO_ZHT] as const;
+export const RULE_LIST = [EnumRuleListKeys.BASE_OLD, EnumRuleListKeys.BASE_V2, EnumRuleListKeys.DEMO_LF2_CHT, EnumRuleListKeys.DEMO_LF2, EnumRuleListKeys.DEMO, EnumRuleListKeys.DEMO_ZHT] as const;
