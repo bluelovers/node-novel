58fd3f9bc2297b4545aedb184573c13d7f5cd305
bluelovers <codelovers@users.sourceforge.net>
2019-08-29 17:11:55 +0800

    [rename] 最強呪族転生

--- lib/fs/move.ts
+++ lib/fs/move.ts
@@ -8,7 +8,7 @@ export function win32_move(src: string, dest: string, options?: fs.MoveOptions)
 		{
 			if (process.platform ==='win32')
 			{
-				if (/dest already exists/i.test(e.message))
+				if (/dest already exists|Source and destination must not be the same/i.test(e.message))
 				{
 					if (src.toLowerCase() === dest.toLowerCase())
 					{
